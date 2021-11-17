133eca11acba93546af56781a13377cd37dde742
宋子慧 <77034643+SongZihui-sudo@users.noreply.github.com>
2021-11-17 18:51:02 +0800

    新增主题标签并改善代码结构

--- Markdown_to_html.h
+++ Markdown_to_html.h
@@ -21,12 +21,13 @@
     int bit_quote = 0;
     int split_line_bit = 0;
     int strong_bit = 0;
+    int bit_theme = 0;
     long long num = 0;
 
     void Welcome();
     void print_file_struct();
     string chose_file_postion(string file);
-    int chose_theme();
+    int chose_theme(string theme_name);
 
     class Markdown_to_html{
         private:
@@ -38,6 +39,7 @@
             char Quote[1] = {'>'}; //引用标签
             char split_line[3] = {'-','-','-'};//分割线标签
             char Strong[2] = {'_','_'};//强调标签
+            char theme_[6] = {'#','t','h','e','m','e'};
             //暂时存放一行的数组
             vector <char> arr;
         public:
@@ -51,11 +53,13 @@
             vector <char> quote(vector <char> quote_arr);//处理引用标签
             vector <char> strong(vector <char> strong_arr);//处理强调语句
         };
+        
     //欢迎界面
     void Welcome(){
         cout<<"Welcome to use e-Conversion html maker!"<<endl;
         cout<<"You can input and enjoy you write!"<<endl;
     }
+
     //输出目录结构
     void print_file_struct(){
         cout<<"e-htmlmaker"<<endl;
@@ -70,6 +74,7 @@
         cout<<"|"<<endl;
         cout<<"————"<<"md: markdown file "<<endl;
     }
+
     //选择存储目录
     string chose_file_postion(string file){
         string h = ".html";
@@ -77,46 +82,60 @@
         int f;
         cin>>f;
         if (f==1){
-            file = "About/" + file + h;
+            file = "About/" + file + h; //目录ABOUT
             return file;
         }
         else if(f==2){
-            file = file + h;
+            file = file + h;//目录
             return file;
         }
         else if(f==3){
             file = "Blog/" + file + h;
-            return file;
+            return file; //目录BLOG
         }
-    else{
+        else{
+            return "1";
+        }
+    }
 
-        return "1";
-    }
-    }
     //选择主题
-    int chose_theme(){
-        string txt = ".txt";
-        cout<<"chose the theme you like:"<<endl;
-        cout<<"type 1---- background is a picture"<<endl;
-        cout<<"type 2---- background is a color"<<endl;
-        cout<<"type 3---- background is a center in the screen"<<endl;
-        string theme_name = " ";
-        int x;        
-        cin>>x;            
-        if(x == 1)
-            theme_name = "theme/" + string("picture_theme") + txt;
-        else if(x == 2)
-            theme_name = "theme/" + string("color_theme") +   txt;
-        else if(x == 3)
-            theme_name = "theme/" + string("text_center") + txt;
+    int chose_theme(vector <char> theme_name){
+        int theme_0_bit = 0;
+        int theme_1_bit = 0;
+        char theme_01[13] = {'p','i','c','t','u','r','e','_','t','h','e','m','e'};
+        char theme_0[11] = {'t','e','x','t','_','c','e','n','t','e','r'};
+        string txt = ".txt";  
+        string na_theme;
+        for (int i = 0; i < theme_name.size(); i++){
+            if (theme_0[i] == theme_name[i]){
+                theme_0_bit = 1;
+            }
+            else{
+                theme_0_bit = 0;
+                break;
+            }
+        }
+        for (int i = 0; i < theme_name.size(); i++){
+            if (theme_01[i] == theme_name[i]){
+                theme_1_bit = 1;
+            }
+            else{   
+                theme_1_bit = 0;
+                break;
+            }
+        }
+        if(theme_1_bit)
+            na_theme = "theme/" + string("picture_theme") + txt;
+        else if(theme_0_bit)
+            na_theme = "theme/" + string("text_center") + txt;
         else;
         ifstream file_in;//切换主题
-        file_in.open(theme_name);
+        file_in.open(na_theme);
             if(file_in){   
                 char t;
                 while ((t = file_in.get()) != EOF){
                     fout<<t;
-                }
+                }        
             }
             else    
                 return 1;
