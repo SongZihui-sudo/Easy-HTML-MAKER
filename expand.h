45f6ea518ad2513fec0034bc7bf6dc700a49e848
宋子慧 <77034643+SongZihui-sudo@users.noreply.github.com>
2021-11-22 22:30:36 +0800

    加入新拓展

--- expand.h
+++ expand.h
@@ -12,12 +12,16 @@
         char index[5] = {'i','n','d','e','x'};
         char Pi_dashboard[13] = {'p','i','-','d','a','s','h','b','o','a','r','d',' '};
         char weather[10] = {'W','e','a','t','h','e','r','.','i','o'};
+        char Next_clond[10] = {'n','e','x','t','c','l','o','u','n','d'}; 
+        char LeaNote[7] = {'l','e','a','n','o','t','e'};
     public:
         vector <char> make_file(vector <char> make);
         expand() = default;  
         vector <char> expand_function(vector <char> expand_arr); //拓展名处理
         vector <char> pi_dashboard_function(vector <char> pi_arr);
         vector <char> Weather_function(vector <char> wea_arr);
+        vector <char> Leanote_function(vector <char> leanote_arr);
+        vector <char> Nextcloud_function(vector <char> nextcloud_arr);
     };
     void print_expand();
 #endif
