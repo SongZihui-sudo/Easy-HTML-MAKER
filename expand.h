227bfc5a726a752e5f9baccdbf3615bd9985c3c5
宋子慧 <77034643+SongZihui-sudo@users.noreply.github.com>
2021-11-22 21:17:15 +0800

    改善代码结构

--- expand.h
+++ expand.h
@@ -11,10 +11,13 @@
         char expand_[7]= {'e','x','p','a','n','d','/'};
         char index[5] = {'i','n','d','e','x'};
         char Pi_dashboard[13] = {'p','i','-','d','a','s','h','b','o','a','r','d',' '};
+        char weather[10] = {'W','e','a','t','h','e','r','.','i','o'};
     public:
+        vector <char> make_file(vector <char> make);
         expand() = default;  
         vector <char> expand_function(vector <char> expand_arr); //拓展名处理
         vector <char> pi_dashboard_function(vector <char> pi_arr);
+        vector <char> Weather_function(vector <char> wea_arr);
     };
     void print_expand();
 #endif
