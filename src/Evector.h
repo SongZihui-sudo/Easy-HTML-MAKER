dc6b89e10b29a5681362335f63bd5bc37730f231
SongZihui-sudo <1751122876@qq.com>
2021-12-30 16:44:42 +0800

    update

---
+++ src/Evector.h
@@ -0,0 +1,24 @@
+#ifndef Evector_h
+#define Evector_h
+namespace Evector
+{
+    class Evector
+    {
+    private:
+        
+    public:
+        Evector();
+        ~Evector();
+    };
+    
+    Evector::Evector()
+    {
+    }
+    
+    Evector::~Evector()
+    {
+    }
+    
+} // namespace Evector
+
+#endif
